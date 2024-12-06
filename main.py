43399e9c9347e80e1ff3232dd7378107b2b2665a
AdamMashaka <mashakaadam123@gmail.com>
2024-12-06 10:31:03 +0300

    window

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#
+#window user
 import subprocess
 
-# Get the list of profiles
+
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 
